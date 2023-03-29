946b8ba16ac06a3669a422b941893028d2b9c99a
Antonio Gálvez Ruz <agr.galvezruz@gmail.com>
2023-03-29 18:40:08 +0200

    Sidebar component

---
+++ src/components/layout-components/sidebar/sidebar-component.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="sidebar">
+    <div class="full-logo">
+      <span class="logo material-icons">stars</span>
+      <div class="logo-name">
+        <span>Gest</span>
+        <span>columba</span>
+      </div>
+    </div>
+
+    <nav>
+      <template v-for="route, index in $router?.options?.routes" :key="index">
+        <ul v-if="!route?.hidden && route.children?.length === 1">
+          <simple-link-component
+            :icon="route.meta.icon"
+            :label="route.name"
+            :parent-path="route.path"
+            :children-path="route.children[0].path" />
+        </ul>
+
+        <group-link-component
+          v-if="!route?.hidden && route.children?.length > 1"
+          :group-label="route.name"
+          :path="route.path"
+          :children="route.children" />
+      </template>
+    </nav>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import SimpleLinkComponent from './simple-link/simple-link-component'
+import GroupLinkComponent from './group-link/group-link-component'
+
+export default defineComponent({
+  components: {
+    SimpleLinkComponent,
+    GroupLinkComponent
+  },
+})
+</script>
