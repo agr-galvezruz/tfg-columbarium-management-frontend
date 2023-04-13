aead111c055147ac0a912c2e6b2034573766795a
Antonio Gálvez Ruz <agr.galvezruz@gmail.com>
2023-04-13 13:40:33 +0200

    Logout button function

--- src/components/layout-components/content-layout/header/header-component.vue
+++ src/components/layout-components/content-layout/header/header-component.vue
@@ -5,9 +5,37 @@
       <span>Antonio Gálvez Ruz</span>
     </div>
 
-    <div class="log-out">
+    <div class="log-out" @click="logout">
       <span>Cerrar sesión </span>
       <span class="logout material-icons">power_settings_new</span>
     </div>
   </section>
 </template>
+
+<script>
+import { useAuthenticationStore } from 'stores/authentication'
+import { updateHeaders } from 'src/boot/axios'
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  setup() {
+    const authenticationStore = useAuthenticationStore()
+    const router = useRouter()
+
+    const logout = async () => {
+      try {
+        await authenticationStore.logout()
+      } finally {
+        sessionStorage.clear()
+        updateHeaders(null)
+        router.replace({ name: 'login' })
+      }
+    }
+
+    return{
+      logout
+    }
+  }
+})
+</script>
