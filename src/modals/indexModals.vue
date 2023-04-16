884d97a0b0c84087bafabfc3df650548f3f8dd3b
Antonio Gálvez Ruz <agr.galvezruz@gmail.com>
2023-04-16 16:28:50 +0200

    Create edit building modal

--- src/modals/indexModals.vue
+++ src/modals/indexModals.vue
@@ -1,10 +1,13 @@
 <template>
+  <create-edit-building-modal />
 </template>
 
 <script>
+import CreateEditBuildingModal from 'src/modals/building/CreateEditBuildingModal'
 
 export default {
   components: {
+    CreateEditBuildingModal,
   }
 }
 </script>
