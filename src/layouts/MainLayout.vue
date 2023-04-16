36612dfe90170dcbf65c00429f4e5cb394026ec9
Antonio Gálvez Ruz <agr.galvezruz@gmail.com>
2023-04-16 16:24:57 +0200

    Dialog component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <toolbar-component v-if="showToolbar" />
   <div class="main-layout">
+    <index-modals />
     <sidebar-component />
     <content-layout-component />
   </div>
@@ -8,12 +9,14 @@
 
 <script>
 import { computed, defineComponent } from 'vue'
+import IndexModals from 'src/modals/IndexModals'
 import ToolbarComponent from 'src/components/toolbar/toolbar-component'
 import SidebarComponent from 'src/components/layout-components/sidebar/sidebar-component'
 import ContentLayoutComponent from 'src/components/layout-components/content-layout/content-layout-component'
 
 export default defineComponent({
   components: {
+    IndexModals,
     ToolbarComponent,
     SidebarComponent,
     ContentLayoutComponent
