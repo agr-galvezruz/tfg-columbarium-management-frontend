907efad321f84f1b6ecb5ae368a754a2b31ad1db
Antonio Gálvez Ruz <agr.galvezruz@gmail.com>
2023-04-16 16:30:32 +0200

    i18n texts

--- src/i18n/es-ES/index.js
+++ src/i18n/es-ES/index.js
@@ -2,6 +2,12 @@
 // so you can safely delete all default props below
 
 export default {
+  routes: {
+    dashboard: 'Dashboard',
+    crypt: 'Cripta',
+    buildings: 'Edificios',
+    rooms: 'Salas'
+  },
   general_texts: {
     edit: 'Editar',
     delete: 'Eliminar',
@@ -9,6 +15,9 @@ export default {
     no_data: 'No se han encontrado registros.',
     pagination_records: 'Registros',
     pagination_of: 'de',
+    cancel: 'Cancelar',
+    delete: 'Eliminar',
+    save: 'Guardar'
   },
   database_messages: {
     incorrect_login_data: 'Correo electrónico o contraseña incorrectos',
@@ -32,7 +41,8 @@ export default {
       no_data: 'Sin datos',
       check_form: 'Revisar los datos introducidos',
       timer_selector: 'Selector de hora',
-      cant_zero: 'El importe debe de ser mayor que cero'
+      cant_zero: 'El importe debe de ser mayor que cero',
+      no_selected: 'Seleccione una opción'
     },
     filter: {
       add_filter: 'Filtrar',
@@ -46,8 +56,61 @@ export default {
   pages: {
     login: {
       email: 'Correo electrónico',
+      email_placeholder: 'Introduzca su correo electrónico',
       password: 'Contraseña',
+      password_placeholder: 'Introduzca su contraseña',
       login: 'Iniciar sesión'
+    },
+    building: {
+      entity: 'Edificio',
+      list_title: 'Edificios',
+      details_title: 'Detalles del edificio',
+      add_building: 'Añadir edificio',
+      internal_code: 'Código interno',
+      internal_code_placeholder: 'Código interno del edificio',
+      name: 'Nombre',
+      name_placeholder: 'Nombre completo del edificio',
+      address: 'Dirección',
+      address_placeholder: 'Dirección del edificio',
+      description: 'Descripción',
+      description_placeholder: 'Descripción sobre el edificio',
+      no_data: 'No se han encontrado edificios.',
+      modal_create_title: 'Añadir edificio',
+      modal_edit_title: 'Editar edificio',
+      modal_delete_title: 'Eliminar edificio',
+      modal_delete_text1: '¿Está seguro que quiere eliminar el edificio',
+      modal_delete_text2: 'Todos los datos que dependen de este edificio serán eliminados.',
+      unique_error_internal_code: 'El código interno introducido ya está en uso.',
+      unique_error_name: 'El nombre del edificio introducido ya está en uso.',
+      successfully_created: 'El edificio ha sido creado correctamente.',
+      successfully_edited: 'El edificio ha sido editado correctamente.',
+      successfully_deleted: 'El edificio ha sido eliminado correctamente.',
+      delete_error: 'El edificio no ha sido encontrado.'
+    },
+    room: {
+      entity: 'Sala',
+      list_title: 'Salas',
+      rooms_building: 'Salas del edificio',
+      add_room: 'Añadir sala',
+      internal_code: 'Código interno',
+      internal_code_placeholder: 'Código de la sala',
+      location: 'Ubicación',
+      location_placeholder: 'Ubicación de la sala',
+      building_id: 'Edificio al que pertenece',
+      building_id_placeholder: 'Seleccione el edificio al que pertenece la sala',
+      description: 'Descripción',
+      description_placeholder: 'Descripción sobre la sala',
+      no_data: 'No se han encontrado salas.',
+      modal_create_title: 'Añadir sala',
+      modal_edit_title: 'Editar sala',
+      modal_delete_title: 'Eliminar sala',
+      modal_delete_text1: '¿Está seguro que quiere eliminar la sala',
+      modal_delete_text2: 'Todos los datos que dependen de esta sala serán eliminados.',
+      unique_error_internal_code: 'El código interno introducido ya está en uso para este edificio.',
+      successfully_created: 'La sala ha sido creada correctamente.',
+      successfully_edited: 'La sala ha sido editada correctamente.',
+      successfully_deleted: 'La sala ha sido eliminada correctamente.',
+      delete_error: 'La sala no ha sido encontrada.'
     },
     person: {
       title: 'Personas',
