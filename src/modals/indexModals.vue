614114eec8a0adbd50960104313cc8bb837aeaf5
Antonio Gálvez Ruz <agr.galvezruz@gmail.com>
2023-04-16 16:33:47 +0200

    Create edit room modal

--- src/modals/indexModals.vue
+++ src/modals/indexModals.vue
@@ -1,16 +1,19 @@
 <template>
   <create-edit-building-modal />
   <delete-building-modal />
+  <create-edit-room-modal />
 </template>
 
 <script>
 import CreateEditBuildingModal from 'src/modals/building/CreateEditBuildingModal'
 import DeleteBuildingModal from 'src/modals/building/DeleteBuildingModal'
+import CreateEditRoomModal from 'src/modals/room/CreateEditRoomModal'
 
 export default {
   components: {
     CreateEditBuildingModal,
     DeleteBuildingModal,
+    CreateEditRoomModal,
   }
 }
 </script>
